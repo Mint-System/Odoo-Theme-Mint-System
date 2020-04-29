7d53171bf2cce484ba0484dc1fa7927b7d9b50c4
Janik von Rotz <login@janikvonrotz.ch>
2020-04-29 17:30:51 +0200

    init

---
+++ __manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name':'Mint System Theme',
+    'description': 'Odoo website theme for Mint System GmbH.',
+    'version': '1.0.0',
+    'author':'Mint System GmbH',
+
+    'data': [
+    ],
+    'category': 'Theme/Creative',
+    'depends': ['website', 'website_theme_install'],
+}
