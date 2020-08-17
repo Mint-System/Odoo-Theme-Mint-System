ef2c1eb22f010c04f29a55eca8fb59e86d729dd6
Janik von Rotz <login@janikvonrotz.ch>
2020-08-17 08:59:56 +0200

    set license and update metadata

--- __manifest__.py
+++ __manifest__.py
@@ -1,15 +1,29 @@
+# -*- coding: utf-8 -*-
 {
-    'name':'Mint System Theme',
-    'description': 'Odoo website theme for Mint System GmbH.',
+    'name': "Mint System Theme",
+
+    'summary': """
+        Odoo website theme for Mint System GmbH.
+    """,
+
+    'description': """
+        Odoo website theme for Mint System GmbH.
+    """,
+    
+    'author': 'Mint System',
+    'website': 'https://www.mint-system.ch',
+    'website': "https://www.mint-system.ch",
+    'license': 'AGPL-3',
+    'category': 'Theme',
+    'version': '0.7.11',
     'sequence': 120,
-    'version': '0.7.10',
-    'author':'Mint System GmbH',
+
+    'depends': ['website_blog'],
+
     'data': [
         'views/layout.xml',
         'views/assets.xml',
         'views/snippets.xml',
         'views/snippet_options.xml'
     ],
-    'category': 'Theme',
-    'depends': ['website_blog'],
 }
