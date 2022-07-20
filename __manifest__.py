2b6534afc4fe0d76c12f367dfe328042a89e9b03
janikvonrotz <contact@janikvonrotz.ch>
2022-07-20 09:27:23 +0200

    update styles

--- __manifest__.py
+++ __manifest__.py
@@ -1,24 +1,19 @@
 {
-    'name': "Mint System Theme",
-
-    'summary': """
+    "name": "Mint System Theme",
+    "summary": """
         Odoo website theme for Mint System GmbH.
     """,
-    
-    'author': 'Mint System GmbH, Odoo Community Association (OCA)',
-    'website': 'https://www.mint-system.ch',
-    'license': 'AGPL-3',
-    'category': 'Theme',
-    'version': '14.0.2.2.1',
-    'sequence': 120,
-
-    'depends': ['website_blog'],
-
-    'data': [
-        'views/layout.xml',
-        'views/assets.xml',
-        'views/snippets.xml',
-        'views/snippet_options.xml',
-        'views/website_templates.xml'
+    "author": "Mint System GmbH, Odoo Community Association (OCA)",
+    "website": "https://www.mint-system.ch",
+    "license": "AGPL-3",
+    "category": "Theme",
+    "version": "14.0.2.2.2",
+    "depends": ["website_blog"],
+    "data": [
+        "views/layout.xml",
+        "views/assets.xml",
+        "views/snippets.xml",
+        "views/snippet_options.xml",
+        "views/website_templates.xml",
     ],
 }
